c8b399fd2c706edba161e8bdafc19d31cad94ad5
AginS <agins.main@gmail.com>
2019-10-23 19:05:52 +0300

    поменял

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     cout << "Введите число " << endl;
     string value;
     cin >> value;
-    if ( value.find("777") <= value.length() ){
+    if ( value.find("777") != value.npos ){
         cout << "Содержится" << endl;
     } else
     {
